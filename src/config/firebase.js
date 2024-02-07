d2835af5543117e9f42554282e9952559b951cf3
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-02-07 17:02:06 +0530

    admins

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,18 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDXIZCRXsek07QRwKhTul51ReA38qnEwP8",
+  authDomain: "shanu-chess.firebaseapp.com",
+  projectId: "shanu-chess",
+  storageBucket: "shanu-chess.appspot.com",
+  messagingSenderId: "27337437521",
+  appId: "1:27337437521:web:bb1fbf490e7f2596c06370",
 };
 
 // Initialize Firebase
