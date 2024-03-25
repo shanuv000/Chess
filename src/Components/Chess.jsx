978ab6e4dd0f0bb9faf4107d6098c517c445f848
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-25 13:52:56 +0530

    resize

--- src/Components/Chess.jsx
+++ src/Components/Chess.jsx
@@ -3,6 +3,8 @@ import moment from "moment";
 import { db } from "../config/firebase";
 import { getDocs, collection } from "firebase/firestore";
 import Loader from "./Loader";
+import { Resizable } from "react-resizable";
+import "../App.css";
 
 const Chess = () => {
   const [chessList, setChessList] = useState([]);
@@ -39,6 +41,11 @@ const Chess = () => {
     return <Loader />;
   }
 
+  const onResize = (event, { element, size }) => {
+    element.style.width = `${size.width}px`;
+    element.style.height = `${size.height}px`;
+  };
+
   return (
     <div className="container mt-4">
       <h2 className="text-center mb-4">Chess Game</h2>
@@ -47,26 +54,31 @@ const Chess = () => {
         {sortChessListByTimestamp().map((chess) => (
           <div key={chess.id} className="col mb-4">
             <div className="card h-100">
-              <div className="card-body">
-                <p className="card-text">
-                  <small className="text-muted">
-                    {chess.timestamp
-                      ? moment(chess.timestamp.toDate()).format("MMM Do YYYY")
-                      : moment().format("MMM Do YYYY")}
-                  </small>
-                </p>
-                <iframe
-                  title={`chess ${chess.id}`}
-                  id={chess.id}
-                  frameBorder={0}
-                  style={{
-                    width: "100%",
-                    border: "none",
-                    height: "600px", // Adjusted height for better visibility
-                  }}
-                  src={`//www.chess.com/emboard?id=${chess.gameNum}`}
-                />
-              </div>
+              {" "}
+              <Resizable
+                width={"100%"}
+                height={500}
+                onResize={onResize}
+                className="resizable-iframe"
+              >
+                <div className="card-body chess-card-body p-0 m-0">
+                  <p className="card-text">
+                    <small className="text-muted">
+                      {chess.timestamp
+                        ? moment(chess.timestamp.toDate()).format("MMM Do YYYY")
+                        : moment().format("MMM Do YYYY")}
+                    </small>
+                  </p>
+
+                  <iframe
+                    title={`chess ${chess.id}`}
+                    id={chess.id}
+                    frameBorder={0}
+                    style={{ width: "100%", height: "100%" }}
+                    src={`//www.chess.com/emboard?id=${chess.gameNum}`}
+                  />
+                </div>
+              </Resizable>
             </div>
           </div>
         ))}
