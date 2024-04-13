343ae84a8bd6bb410f96e037786c6c552a7cd666
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-04-13 22:10:57 +0530

    update apis

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,12 +9,12 @@ const firebaseConfig = {
   // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   // appId: process.env.REACT_APP_APP_ID,
-  apiKey: "AIzaSyDXIZCRXsek07QRwKhTul51ReA38qnEwP8",
-  authDomain: "shanu-chess.firebaseapp.com",
-  projectId: "shanu-chess",
-  storageBucket: "shanu-chess.appspot.com",
-  messagingSenderId: "27337437521",
-  appId: "1:27337437521:web:bb1fbf490e7f2596c06370",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
